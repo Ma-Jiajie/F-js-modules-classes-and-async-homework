e40bd9c41c7f25f86c9a3c9c9ff7da298a8d2438
jiajie <jiajie.ma@thoughtworks.com>
2020-10-16 09:06:19 +0800

    jiajie: finish the first&second practice.

--- src/practice1/index.js
+++ src/practice1/index.js
@@ -1,1 +1,7 @@
+import * as constant from "./constant";
+import getURL from "./utils";
 
+const url = getURL(constant.ADDRESS, constant.PORT, constant.PATH);
+const region = constant.REGION;
+
+export { url, region };
--- src/practice2/Student.js
+++ src/practice2/Student.js
@@ -1,1 +1,14 @@
-export class Student {}
+import Person from "./Person";
+
+export default class Student extends Person {
+  constructor(name, className) {
+    super(name);
+    this.name = name;
+    this.className = className;
+  }
+
+  study = () => {
+    console.log(`${this.name} is moving`);
+    console.log(`${this.name} is studying in ${this.className}`);
+  };
+}
